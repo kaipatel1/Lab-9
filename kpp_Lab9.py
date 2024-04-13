95f1a5bc50d92e44dcc19ae4e656778654379b84
jackplo <jackplo53@gmail.com>
2024-04-13 19:55:07 -0400

    Decode Function

--- kpp_Lab9.py
+++ kpp_Lab9.py
@@ -1,12 +1,19 @@
 #Kai Patel
+
 def encode(password):
     result = ""
     for i in password:
         result += str(int(i) + 3)
     return result
 
+def decode(password):
+    result = ""
+    for j in password:
+        result += str(int(j) - 3)
+    return result
 
 def main():
+    password = ""
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         selection = input("Please enter an option: ")
@@ -15,7 +22,8 @@ def main():
             password = encode(input("Please enter an option: "))
             print("Your password has been encoded and stored!")
         elif selection == "2":
-            pass
+            result = decode(password)
+            print(f"The encoded password is {password}, and the original password is {result}.")
         elif selection == "3":
             break
 
